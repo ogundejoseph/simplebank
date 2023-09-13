36ac4d5104976c28e35752e26855e4a6351ce1ad
ogundejoseph <ogundejoseph57@gmail.com>
2023-09-13 23:57:36 +0300

    create account test

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -4,14 +4,15 @@ import (
 	"context"
 	"testing"
 
+	"github.com/ogundejoseph/simplebank/util"
 	"github.com/stretchr/testify/require"
 )
 
 func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams {
-		Owner: "Joe",
-		Balance: 100,
-		Currency: "KES",
+		Owner: util.RandomOwner(),
+		Balance: util.RandomMoney(),
+		Currency: util.RandomCurrency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
